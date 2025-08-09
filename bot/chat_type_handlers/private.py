2f0b50bef61d8f4adbccd23a027c4410191306e2
MaxMavr <maxmavr2005@gmail.com>
2025-08-09 13:13:46 +0300

    Fix: bot's reaction to pinning the message

--- bot/chat_type_handlers/private.py
+++ bot/chat_type_handlers/private.py
@@ -6,6 +6,9 @@ from bot.keyboards import inline as ikb
 
 
 async def handler(message: Message):
+    if message.pinned_message:
+        return
+
     if message.text:
         if await EditFilter.check(message.from_user.id):
             await message.answer(text=await format_song_line(message.text),
@@ -16,4 +19,5 @@ async def handler(message: Message):
                                  disable_web_page_preview=True,
                                  reply_markup=ikb.suggest_post(message.from_user.id, message.message_id))
         return
+
     await message.answer(text=PHRASES_RU.answers.unknown)
