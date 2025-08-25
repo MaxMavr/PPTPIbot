12cb11530ed38e1bc4fc0c1787fff9feb557dda5
MaxMavr <maxmavr2005@gmail.com>
2025-08-25 22:29:43 +0300

    Add: ChatActionSender to format song lyrics

--- bot/chat_type_handlers/private.py
+++ bot/chat_type_handlers/private.py
@@ -1,23 +1,27 @@
 from aiogram.types import Message
+from aiogram.utils.chat_action import ChatActionSender
+
 from bot.bot_utils.filters import EditFilter
+from config import bot
 from phrases import PHRASES_RU
 from utils.format_song_line import format_song_line
 from bot.keyboards import inline as ikb
 
 
 async def handler(message: Message):
-    if message.pinned_message:
-        return
+    async with ChatActionSender.typing(bot=bot, chat_id=message.chat.id):
+        if message.pinned_message:
+            return
 
-    if message.text:
-        if await EditFilter.check(message.from_user.id):
-            await message.answer(text=await format_song_line(message.text),
-                                 disable_web_page_preview=True,
-                                 reply_markup=ikb.publish_post(message.from_user.id))
-        else:
-            await message.answer(text=await format_song_line(message.text),
-                                 disable_web_page_preview=True,
-                                 reply_markup=ikb.suggest_post(message.from_user.id, message.message_id))
-        return
+        if message.text:
+            if await EditFilter.check(message.from_user.id):
+                await message.answer(text=await format_song_line(message.text),
+                                     disable_web_page_preview=True,
+                                     reply_markup=ikb.publish_post(message.from_user.id))
+            else:
+                await message.answer(text=await format_song_line(message.text),
+                                     disable_web_page_preview=True,
+                                     reply_markup=ikb.suggest_post(message.from_user.id, message.message_id))
+            return
 
-    await message.answer(text=PHRASES_RU.answers.unknown)
+        await message.answer(text=PHRASES_RU.answers.unknown)
