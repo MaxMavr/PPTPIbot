390c82139d65ad6f481d886987b2e5a863f35ec0
MaxMavr <maxmavr2005@gmail.com>
2025-08-26 00:44:47 +0300

    Add: lyrics in inline admin song

--- bot/handlers/inline.py
+++ bot/handlers/inline.py
@@ -6,7 +6,7 @@ from utils.format_song_line import format_song_line
 from phrases import PHRASES_RU
 from utils.format_string import make_song_lyrics_message
 from utils.links import make_yandex_song_link
-from utils.music_yandex import get_admin_song
+from utils.music_yandex import get_admin_song, get_random_song_lines
 
 router = Router()
 
@@ -18,11 +18,18 @@ async def inline_get_photo(query: types.InlineQuery):
     if not text:
         return await query.answer([], cache_time=1, is_personal=True)
 
-    if text.strip() in ['as', '/', '@']:
+    if text in ['as', '/', '@']:
         song, artists, song_id = await get_admin_song()
         title = song
         description = artists
-        text = make_song_lyrics_message(song=song, artist=artists, link=make_yandex_song_link(song_id))
+
+        lyrics = None
+        if text == 'as':
+            lyrics = await get_random_song_lines(song_id)
+
+        text = make_song_lyrics_message(song=song, artist=artists,
+                                        link=make_yandex_song_link(song_id), lyrics=lyrics)
+
     else:
         title = PHRASES_RU.title.inline
         description = PHRASES_RU.footnote.inline
