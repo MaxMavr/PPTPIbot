b79247607f0b46f8a6935827e488a969fd445b7f
MaxMavr <maxmavr2005@gmail.com>
2025-08-25 22:28:37 +0300

    Add: more to phasalo_drollery aliases

--- bot/handlers/phasalo_drollery.py
+++ bot/handlers/phasalo_drollery.py
@@ -11,6 +11,6 @@ async def _(message: Message):
     await message.answer(text=PHRASES_RU.answers.welcome)
 
 
-@router.message(F.text.lower().in_(['мяу', 'мау', 'мив', 'мав']))
+@router.message(F.text.lower().in_(['мяу', 'мау', 'мив', 'мав', 'ку', 'кря', 'мрр', 'пиу', 'пау', 'пум', 'квак']))
 async def _(message: Message):
     await default.cmd_admin_song(message)
