8490120ebcddc40f39cbc872acb855c0592ba688
MaxMavr <maxmavr2005@gmail.com>
2025-11-01 04:59:55 +0300

    Refactor: improved error handling in the bot (Add: error.processing_failed)

--- bot/chat_type_handlers/private.py
+++ bot/chat_type_handlers/private.py
@@ -13,15 +13,19 @@ async def handler(message: Message):
         if message.pinned_message:
             return
 
-        if message.text:
-            if await EditFilter.check(message.from_user.id):
-                await message.answer(text=await format_song_line(message.text),
-                                     disable_web_page_preview=True,
-                                     reply_markup=ikb.publish_post(message.from_user.id))
-            else:
-                await message.answer(text=await format_song_line(message.text),
-                                     disable_web_page_preview=True,
-                                     reply_markup=ikb.suggest_post(message.from_user.id, message.message_id))
-            return
+        if not message.text:
+            await message.answer(text=PHRASES_RU.answers.unknown)
 
-        await message.answer(text=PHRASES_RU.answers.unknown)
+        msg_text = await format_song_line(message.text)
+
+        if not msg_text:
+            await message.answer(text=PHRASES_RU.error.processing_failed)
+
+        if await EditFilter.check(message.from_user.id):
+            await message.answer(text=msg_text,
+                                 disable_web_page_preview=True,
+                                 reply_markup=ikb.publish_post(message.from_user.id))
+        else:
+            await message.answer(text=msg_text,
+                                 disable_web_page_preview=True,
+                                 reply_markup=ikb.suggest_post(message.from_user.id, message.message_id))
