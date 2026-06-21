41cfcfeddc6ad54b603237b6c9fa6bea297a8143
yanToples <scam@nik67.ru>
2026-06-22 01:22:50 +0300

    refactor(config): make CHANNEL_ID env-driven

--- config/const.py
+++ config/const.py
@@ -1,9 +1,13 @@
+import os
 from pathlib import Path
+
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
 
 BASE_DIR = Path(__file__).parent.parent
 
 USERS_PER_PAGE = 15
 QUERIES_PER_PAGE = 6
 
-# CHANNEL_ID: str = '@post_pop_trah_punk_indie'
-CHANNEL_ID: str = '@phasalotest'
+CHANNEL_ID: str = os.getenv('CHANNEL_ID') or '@phasalotest'
